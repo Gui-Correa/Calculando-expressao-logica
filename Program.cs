48820c787ea5afe5fbafd12a0c198cd467d50076
Gui-Correa <71907354+Gui-Correa@users.noreply.github.com>
2020-10-02 22:29:37 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercicio_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int w, x, y, z;
+            bool a;
+            Console.WriteLine("digite o valor de w: ");
+            w = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("digite o valor de x: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("digite o valor de y: ");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("digite o valor de z: ");
+            z = Convert.ToInt32(Console.ReadLine());
+
+            a = ((x >= y) && (z <= x)) || ((x == w) && (y == z)) || (!(x != w));
+
+            Console.WriteLine("O resultado da expressão é: " + a);
+            Console.ReadKey();
+
+        }
+    }
+}
